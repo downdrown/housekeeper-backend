c8a99e9db15fb206cc3b947f8ebeba489eab395d
Manfred Huber <manfred.huber@downdrown.at>
2020-12-03 23:12:20 +0100

    adds encoding configuration bean

---
+++ src/main/java/at/downdrown/housekeeper/config/EncodingConfig.java
@@ -0,0 +1,30 @@
+package at.downdrown.housekeeper.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Configuration class that produces encoding related configuration beans.
+ *
+ * @author Manfred Huber
+ */
+@Configuration
+public class EncodingConfig {
+
+    /**
+     * Produces a {@link CharacterEncodingFilter} that forcibly encodes
+     * requests and responses to UTF-8 to prevent encoding related issues.
+     *
+     * @return the configured {@link CharacterEncodingFilter}
+     */
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        final CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding(StandardCharsets.UTF_8.name());
+        filter.setForceEncoding(true); // force for request & response
+        return filter;
+    }
+}

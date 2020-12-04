d478a44d744a8248e478ddd6651560d81d12a25f
Manfred Huber <manfred.huber@downdrown.at>
2020-12-04 02:05:23 +0100

    adds javadoc to ControllerExceptionHandler; fixes typo

--- src/main/java/at/downdrown/housekeeper/controller/ControllerExceptionHandler.java
+++ src/main/java/at/downdrown/housekeeper/controller/ControllerExceptionHandler.java
@@ -7,13 +7,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * Global exception handler that intercepts rest controller calls, checks
+ * for specific Exceptions and returns corresponding {@link ResponseEntity} instances.
+ *
+ * @author Manfred Huber
+ */
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<Void> onDataAccessException(DataAccessException exception) {
-        log.error("DataAccessException occured, aborting request!", exception);
+        log.error("DataAccessException occurred, aborting request!", exception);
         return ResponseEntity
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .build();

3727db53727195b3bbbe6f0348ba39f46adcf393
Manfred Huber <manfred.huber@downdrown.at>
2020-12-04 02:07:35 +0100

    removes unused import

--- src/test/java/at/downdrown/housekeeper/controller/UserControllerTest.java
+++ src/test/java/at/downdrown/housekeeper/controller/UserControllerTest.java
@@ -20,7 +20,6 @@ import java.nio.charset.StandardCharsets;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;

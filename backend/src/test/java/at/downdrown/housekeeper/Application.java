60561e413c27855c700c49d255bba326575593a1
Manfred Huber <manfred.huber@downdrown.at>
2020-12-06 21:18:28 +0100

    removes unnecessary Application classes from test scope

--- backend/src/test/java/at/downdrown/housekeeper/Application.java
+++
@@ -1,8 +0,0 @@
-package at.downdrown.housekeeper;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application {
-    // no-op just needed to have a context during unit-test
-}

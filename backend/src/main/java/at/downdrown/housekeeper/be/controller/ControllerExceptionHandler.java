edf3556442288f9c12625d4078d23da44ea51697
Manfred Huber <manfred.huber@downdrown.at>
2020-12-06 02:55:35 +0100

    updates ControllerExceptionHandler

--- backend/src/main/java/at/downdrown/housekeeper/be/controller/ControllerExceptionHandler.java
+++ backend/src/main/java/at/downdrown/housekeeper/be/controller/ControllerExceptionHandler.java
@@ -1,11 +1,15 @@
 package at.downdrown.housekeeper.be.controller;
 
+import at.downdrown.housekeeper.api.exception.ModelConflictException;
+import at.downdrown.housekeeper.api.exception.ModelNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.function.BiConsumer;
 
 /**
  * Global exception handler that intercepts rest controller calls, checks
@@ -17,11 +21,39 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+    private static final BiConsumer<String, Throwable> DEBUG = log::debug;
+    private static final BiConsumer<String, Throwable> WARN = log::warn;
+    private static final BiConsumer<String, Throwable> ERROR = log::error;
+
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<Void> onDataAccessException(DataAccessException exception) {
-        log.error("DataAccessException occurred, aborting request!", exception);
+        log(ERROR, exception);
         return ResponseEntity
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .build();
+    }
+
+    @ExceptionHandler(ModelConflictException.class)
+    public ResponseEntity<Void> onModelConflictException(ModelConflictException exception) {
+        log(WARN, exception);
+        return ResponseEntity
+            .status(HttpStatus.CONFLICT)
+            .build();
+    }
+
+    @ExceptionHandler(ModelNotFoundException.class)
+    public ResponseEntity<Void> onModelNotFoundException(ModelNotFoundException exception) {
+        log(DEBUG, exception);
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .build();
+    }
+
+    /** Utility method that logs that the request has been aborted with a specific level. */
+    private static void log(final BiConsumer<String, Throwable> logMethod, final Throwable exception) {
+        if (exception != null) {
+            final String logString = String.format("%s occurred, aborting request!", exception.getClass().getSimpleName());
+            logMethod.accept(logString, exception);
+        }
     }
 }

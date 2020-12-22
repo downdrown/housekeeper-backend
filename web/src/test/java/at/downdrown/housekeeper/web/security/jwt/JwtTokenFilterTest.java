e59772d94c3ad29c51bb1e52488b5123b281f880
Manfred Huber <manfred.huber@downdrown.at>
2020-12-22 11:39:04 +0100

    adds JwtTokenFilter to be able to use JWTs as authentication mechanism

---
+++ web/src/test/java/at/downdrown/housekeeper/web/security/jwt/JwtTokenFilterTest.java
@@ -0,0 +1,106 @@
+package at.downdrown.housekeeper.web.security.jwt;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @see JwtTokenFilter
+ * @author Manfred Huber
+ */
+public class JwtTokenFilterTest {
+
+    private JwtProvider mockJwtProvider;
+    private JwtTokenFilter jwtTokenFilter;
+    private HttpServletRequest mockHttpServletRequest;
+    private HttpServletResponse mockHttpServletResponse;
+    private FilterChain mockFilterChain;
+
+    @BeforeEach
+    void setUp() {
+        SecurityContextHolder.clearContext();
+        mockJwtProvider = mock(JwtProvider.class);
+        jwtTokenFilter = new JwtTokenFilter(mockJwtProvider);
+        mockHttpServletRequest = mock(HttpServletRequest.class);
+        mockHttpServletResponse = mock(HttpServletResponse.class);
+        mockFilterChain = mock(FilterChain.class);
+    }
+
+    @Test
+    void shouldFilterAndSucceed() throws ServletException, IOException {
+
+        when(mockHttpServletRequest.getHeader(eq("Authorization")))
+            .thenReturn("Bearer my.bearer.token");
+
+        when(mockJwtProvider.verify(eq("my.bearer.token")))
+            .thenReturn(new User("user", "password", Collections.emptySet()));
+
+        jwtTokenFilter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(mockFilterChain).doFilter(mockHttpServletRequest, mockHttpServletResponse);
+
+        assertThat(SecurityContextHolder.getContext().getAuthentication())
+            .isNotNull()
+            .isInstanceOf(UsernamePasswordAuthenticationToken.class)
+            .extracting(Authentication::getPrincipal)
+            .isInstanceOf(User.class);
+    }
+
+    @Test
+    void shouldFilterAndFailByNoToken() throws ServletException, IOException {
+
+        when(mockHttpServletRequest.getHeader(eq("Authorization")))
+            .thenReturn(null);
+
+        jwtTokenFilter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(mockFilterChain).doFilter(mockHttpServletRequest, mockHttpServletResponse);
+
+        assertThat(SecurityContextHolder.getContext().getAuthentication())
+            .isNull();
+    }
+
+    @Test
+    void shouldFilterAndFailByAuthenticationException() {
+
+        when(mockHttpServletRequest.getHeader(eq("Authorization")))
+            .thenReturn("Bearer my.bearer.token");
+
+        when(mockJwtProvider.verify(eq("my.bearer.token")))
+            .thenThrow(CredentialsExpiredException.class);
+
+        assertThrows(AccessDeniedException.class, () -> jwtTokenFilter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain));
+    }
+
+    @Test
+    void shouldFilterAndFailByExistingAuthentication() throws ServletException, IOException {
+
+        // given
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("user", "password"));
+
+        // when
+        jwtTokenFilter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+
+        // then
+        verify(mockJwtProvider, never()).verify(any());
+    }
+}

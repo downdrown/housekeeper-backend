ad3689076240b6e133b8138202e862a7969d5cdf
Manfred Huber <manfred.huber@downdrown.at>
2020-12-23 01:10:42 +0100

    refactors WebSecurityConfigurationProd

--- web/src/main/java/at/downdrown/housekeeper/web/config/WebSecurityConfigurationProd.java
+++ web/src/main/java/at/downdrown/housekeeper/web/config/WebSecurityConfigurationProd.java
@@ -1,15 +1,21 @@
 package at.downdrown.housekeeper.web.config;
 
+import at.downdrown.housekeeper.web.security.jwt.JwtTokenFilter;
+import at.downdrown.housekeeper.web.security.jwt.TokenEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.firewall.HttpFirewall;
 
 /**
@@ -23,17 +29,19 @@ import org.springframework.security.web.firewall.HttpFirewall;
 public class WebSecurityConfigurationProd extends WebSecurityConfigurerAdapter {
 
     private final HttpFirewall httpFirewall;
-    private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenFilter jwtTokenFilter;
 
     @Autowired
-    public WebSecurityConfigurationProd(
-        final HttpFirewall httpFirewall,
-        final PasswordEncoder passwordEncoder,
-        final UserDetailsService userDetailsService) {
+    public WebSecurityConfigurationProd(final HttpFirewall httpFirewall,
+                                        final UserDetailsService userDetailsService,
+                                        final PasswordEncoder passwordEncoder,
+                                        final JwtTokenFilter jwtTokenFilter) {
         this.httpFirewall = httpFirewall;
-        this.passwordEncoder = passwordEncoder;
         this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenFilter = jwtTokenFilter;
     }
 
     @Override
@@ -44,11 +52,19 @@ public class WebSecurityConfigurationProd extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
+            .cors().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .authorizeRequests()
+            .antMatchers(TokenEndpoint.URL_PATTERN)
+            .permitAll()
+        .and()
             .authorizeRequests()
             .anyRequest()
             .fullyAuthenticated()
         .and()
-            .httpBasic();
+            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Autowired
@@ -56,5 +72,12 @@ public class WebSecurityConfigurationProd extends WebSecurityConfigurerAdapter {
         auth
             .userDetailsService(userDetailsService)
             .passwordEncoder(passwordEncoder);
+    }
+
+    /** Exposes the {@link AuthenticationManager} as Spring bean. */
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }

645eccc51cc9376ce5cb3f25adb47d3ac255d1b3
Manfred Huber <manfred.huber@downdrown.at>
2020-12-24 20:26:20 +0100

    merges create-credentials.sql into create-users.sql

--- api/src/test/java/at/downdrown/housekeeper/TestBase.java
+++ api/src/test/java/at/downdrown/housekeeper/TestBase.java
@@ -13,14 +13,8 @@ public abstract class TestBase {
     protected static final String CREATE_USERS_SQL = "/sql/create-users.sql";
 
     /**
-     * Creates 3 credentials - one for each user.
-     * Requires {@link at.downdrown.housekeeper.TestBase#CREATE_USERS_SQL} to be executed first.
-     */
-    protected static final String CREATE_CREDENTIALS_SQL = "/sql/create-credentials.sql";
-
-    /**
      * Creates 3 avatars - one for each user.
-     * Requires {@link at.downdrown.housekeeper.TestBase#CREATE_USERS_SQL} to be executed first.
+     * Requires {@link TestBase#CREATE_USERS_SQL} to be executed first.
      */
     protected static final String CREATE_AVATARS_SQL = "/sql/create-avatars.sql";
 
--- api/src/test/resources/sql/create-credentials.sql
+++
@@ -1,4 +0,0 @@
--- Insert mock credentials
-insert into credentials (id, users_fk, password) values (10, 10, '$2a$10$umThkjstS0ZuSaUmvyxkfucpi2wHH3q6y/8U.Sit8SNjVOAPeMjgi'); -- admin
-insert into credentials (id, users_fk, password) values (20, 20, '$2a$10$umThkjstS0ZuSaUmvyxkfucpi2wHH3q6y/8U.Sit8SNjVOAPeMjgi'); -- user
-insert into credentials (id, users_fk, password) values (30, 30, '$2a$10$umThkjstS0ZuSaUmvyxkfucpi2wHH3q6y/8U.Sit8SNjVOAPeMjgi'); -- guest

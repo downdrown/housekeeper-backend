db63718fdb0d725783d2169e9d0af5fcba284d85
Manfred Huber <manfred.huber@downdrown.at>
2020-12-30 18:56:25 +0100

    refactors WebSecurityConfigurationDev

--- web/src/main/java/at/downdrown/housekeeper/web/config/WebSecurityConfigurationDev.java
+++ web/src/main/java/at/downdrown/housekeeper/web/config/WebSecurityConfigurationDev.java
@@ -2,19 +2,29 @@ package at.downdrown.housekeeper.web.config;
 
 import at.downdrown.housekeeper.api.Role;
 import at.downdrown.housekeeper.api.service.GrantedAuthorityService;
+import at.downdrown.housekeeper.web.security.jwt.TokenEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.List;
 
 /**
  * Web-Security related configuration bean for development.
@@ -48,14 +58,23 @@ public class WebSecurityConfigurationDev extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
-            .csrf().disable()
-            .cors().disable()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+            .antMatchers(TokenEndpoint.URL_PATTERN).permitAll()
+        .and()
             .authorizeRequests()
             .anyRequest()
             .fullyAuthenticated()
         .and()
-            .httpBasic();
+            .httpBasic()
+        .and()
+            .csrf().disable()
+            .cors().configurationSource(corsConfigurationSource())
+        .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
     }
 
     @Autowired
@@ -73,5 +92,16 @@ public class WebSecurityConfigurationDev extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("content-type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
